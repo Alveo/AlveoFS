10974e5fd0a1c9724f5c4980d895d29cc1320d65
Karl Li <karl.lee.2004@gmail.com>
2018-05-15 10:57:41 +1000

    source file check in

---
+++ httpfs.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+import sys
+import logging
+
+import argparse
+import requests
+import six
+
+from fuse import FUSE, Operations, FuseOSError, ENOENT
+from parser import Directory, File
+
+
+class HTTPfs(Operations):
+    def __init__(self, root, verify_ssl=True):
+        self.root = root
+        self.log = logging.getLogger(__name__)
+        self.readdir_cache = {}
+        self.attr_cache = {}
+        self.file_cache = {}
+
+        # append headers
+        s = requests.Session()
+        s.headers.update({'X-API-Key': 'fxss5G7NxD472koixm7r', 'Accept': 'application/json'})
+
+        self.session = s
+        if not verify_ssl:
+            self.log.warn("Disabling SSL verification!")
+            self.session.verify = False
+
+    def readdir(self, path, fh):
+        path = path.strip("/")
+        path = six.text_type(path)
+
+        self.log.debug(u"[READDIR] Reading path {}".format(path))
+        if path not in self.readdir_cache.keys():
+            self.readdir_cache[path] = Directory(self.root, path, self.session).contents()
+
+        return [x[0] for x in self.readdir_cache[path]]
+
+    def read(self, path, length, offset, fh):
+        path = path.strip("/")
+        path = six.text_type(path)
+
+        self.log.debug(u"[READ] Reading path {}, {} bytes from {}".format(path, length, offset))
+        if path not in self.file_cache.keys():
+            self.file_cache[path] = File(self.root, path, self, self.session)
+
+        return self.file_cache[path].read(length, offset)
+
+    def getattr(self, path, fh=None):
+        path = path.strip("/")
+        path = six.text_type(path)
+
+        self.log.debug(u"[GETATTR] Path {}".format(path))
+        if path not in self.attr_cache.keys():
+            try:
+                if path not in self.file_cache.keys():
+                    self.file_cache[path] = File(self.root, path, self, self.session)
+                self.attr_cache[path] = self.file_cache[path].attributes()
+            except FuseOSError:
+                self.attr_cache[path] = None
+                raise FuseOSError(ENOENT)
+
+        if self.attr_cache[path] is not None:
+            return self.attr_cache[path]
+        else:
+            raise FuseOSError(ENOENT)
+
+    # Disable unused operations:
+    access = None
+    flush = None
+    getxattr = None
+    listxattr = None
+    open = None
+    opendir = None
+    release = None
+    releasedir = None
+    statfs = None
+
+
+if __name__ == '__main__':
+    FORMAT = "%(created)f - %(thread)d (%(name)s) - [%(levelname)s] %(message)s"
+    logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+    p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    p.add_argument("http_resource", help="Target web directory index")
+    p.add_argument("mountpoint", help="Target directory")
+    p.add_argument("--foreground", action="store_true", help="Do not fork into background")
+    p.add_argument("--debug", action="store_true", help="Enable debug logging")
+    p.add_argument("--nothreads", action="store_true", help="Disable fuse threads")
+    p.add_argument("--no_ssl_verify", action="store_true", help="Disable SSL Verification")
+    p.add_argument("--allow_other", action="store_true", help="Allow users other than the one running the command "
+                                                              "to access the directory.")
+
+    p.add_argument("-o", "--options", type=str, default="", help="Mount-style variant of the above options "
+                                                                 "(e.g. -o debug,allow_other")
+
+    args = vars(p.parse_args(sys.argv[1:]))
+
+    fsroot = six.text_type(args.pop("http_resource").strip("/"))
+    mountpoint = args.pop("mountpoint")
+
+    fuse_kwargs = {
+        'nothreads': True if args.pop("nothreads") else False,
+        'foreground': True if args.pop("foreground") else False,
+        'debug': True if args.pop("debug") else False,
+        'allow_other': True if args.pop("allow_other") else False,
+    }
+
+    # o_args_list = [x.strip() for x in args.pop("o").split(",")]
+    # o_args = {}
+    # for x in o_args_list:
+    #     xs = [y.strip() for y in x.split("=")]
+    #     if len(xs) > 1:
+    #         fuse_kwargs[xs[0]] = xs[1:]
+    #     else:
+    #         fuse_kwargs[x] = True
+
+    if fuse_kwargs['debug']:
+        logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+
+    FUSE(HTTPfs(fsroot, verify_ssl=False if args.pop("no_ssl_verify") else True), mountpoint, **fuse_kwargs)
---
+++ parser.py
@@ -0,0 +1,197 @@
+import logging
+import six
+import time
+import json
+
+from errno import ENOENT
+from stat import S_IFDIR, S_IFREG
+from datetime import datetime
+from collections import defaultdict
+from fuse import FuseOSError, EIO
+
+
+class Directory:
+    def __init__(self, root, path, session):
+        self.root = root
+        self.path = path
+        self.session = session
+        self.log = logging.getLogger("Directory")
+        self.log.debug(u"[INIT] Loading directory {}/{}".format(root, path))
+
+    def contents(self):
+        """
+        Give the contents of the directory
+        :return: List of Entities that are in the directory
+        :rtype: list
+        """
+        contents = [(".", True), ("..", True)]
+
+        # # Do a request, and run it through an HTML parser.
+        # response = self.session.get(u"{}/{}/".format(self.root, self.path))
+        # parsed = BeautifulSoup(response.text, 'html.parser')
+        #
+        # # Find all of the entity elements, remove the cruft
+        # for x in parsed.find_all("tr"):
+        #     # if x.td is not None and x.td.img['alt'] != "[PARENTDIR]":
+        #     #     is_dir = x.td.img['alt'] == "[DIR]"
+        #     #     contents.append((x.find_all('td')[1].a.string.strip("/"), is_dir))
+        #     tds = x.find_all('td')
+        #     if len(tds) > 0:
+        #         name = tds[1].a.string
+        #         is_dir = name.endswith('/')
+        #         contents.append((name.strip("/"), is_dir))
+        #
+        # return contents
+
+        # Do a request and parse JSON
+        response = json.loads((self.session.get(u"{}/{}/".format(self.root, self.path))).text)
+
+        if 'directories' in response:
+            directory_arr = response['directories']
+            for d in directory_arr:
+                name = d.split('/')[-1]
+                is_dir = True
+                contents.append((name, is_dir))
+
+        if 'files' in response:
+            file_arr = response['files']
+            for f in file_arr:
+                name = f.split('/')[-1]
+                is_dir = False
+                contents.append((name, is_dir))
+
+        return contents
+
+
+class File:
+    def __init__(self, root, path, httpfs, session):
+        self.root = root
+        self.path = path
+        self.session = session
+        self.log = logging.getLogger("File")
+        self.log.debug(u"[INIT] Loading file {}/{}".format(root, path))
+        self.readbuffer = defaultdict(lambda: None)
+        self.is_filtered = False
+
+        print "----- Loading file root[{}], path[{}]".format(root, path)
+
+        # filter file
+        if self.filter_path():
+            self.is_dir = False
+            self.mtime = time.time()
+            self.size = 0
+            self.is_filtered = True
+            return
+
+        # Determine if this is a directory
+        parent_dir = "/".join(self.path.split("/")[:-1])
+        filename = self.path.split("/")[-1]
+        if parent_dir not in httpfs.readdir_cache.keys():
+            httpfs.readdir_cache[parent_dir] = Directory(self.root, parent_dir, self.session).contents()
+
+        dirs = [six.text_type(x[0]) for x in httpfs.readdir_cache[parent_dir] if x[1]]
+        self.is_dir = (six.text_type(filename) in dirs) or six.text_type(filename) == six.text_type("")
+
+        # Determine file size
+        self.url = u"{}/{}{}".format(self.root, self.path, "/" if self.is_dir else "")
+        self.r = self.session.head(self.url, allow_redirects=True)
+        if self.r.status_code == 200:
+            try:
+                self.size = int(self.r.headers['Content-Length'])
+            except KeyError:
+                self.size = 0
+
+            try:
+                mtime_string = self.r.headers["Last-Modified"]
+                # self.mtime = time.mktime(datetime.strptime(mtime_string, "%a, %d %b %Y %H:%M:%S %Z").timetuple())
+                self.mtime = time.time()
+            except KeyError:
+                self.mtime = time.time()
+        else:
+            self.log.info(u"[INIT] Non-200 code while getting {}: {}".format(self.url, self.r.status_code))
+            self.size = 0
+
+    def read(self, length, offset):
+        """
+        Reads the file.
+        :param length: The length to read
+        :param offset: The offset to start at
+        :return: The file's bytes
+        """
+        self.log.debug(u"[READ] Reading file {}/{}".format(self.root, self.path))
+        url = u"{}/{}".format(self.root, self.path)
+
+        # Calculate megabyte-section this offset/length is in
+        mb_start = (offset // 1024) // 1024
+        mb_end = ((offset + length) // 1024) // 1024
+        offset_from_mb = (((offset // 1024) % 1024) * 1024) + (offset % 1024)
+        self.log.debug(u"Calculated MB_Start {} MB_End {} Offset from MB: {}".format(mb_start, mb_end, offset_from_mb))
+        if mb_start == mb_end:
+            self.log.debug(u"Readbuffer filled for mb_start? {}".format(self.readbuffer[mb_start] is not None))
+            if self.readbuffer[mb_start] is None:
+                # Fill buffer for this MB
+                bytesRange = u'{}-{}'.format(mb_start * 1024 * 1024, (mb_start * 1024 * 1024) + (1023 * 1024))
+                self.log.debug(u"Fetching byte range {}".format(bytesRange))
+                headers = {'range': 'bytes=' + bytesRange}
+                r = self.session.get(url, headers=headers)
+                if r.status_code == 200 or r.status_code == 206:
+                    self.readbuffer[mb_start] = r.content
+                    # noinspection PyTypeChecker
+                    self.log.debug(u"Read {} bytes.".format(len(self.readbuffer[mb_start])))
+                else:
+                    self.log.info(u"[INIT] Non-200 code while getting {}: {}".format(url, r.status_code))
+                    raise FuseOSError(EIO)
+
+            self.log.debug(u"Returning indices {} to {}".format(offset_from_mb, offset_from_mb + length))
+            return self.readbuffer[mb_start][offset_from_mb:offset_from_mb + length]
+        else:
+            self.log.debug(u"Offset/Length spanning multiple MB's. Fetching normally")
+            # Spanning multiple MB's, just get it normally
+            # Set range
+            bytesRange = u'{}-{}'.format(offset, min(self.size, offset + length - 1))
+            self.log.debug(u"Fetching byte range {}".format(bytesRange))
+            headers = {'range': 'bytes=' + bytesRange}
+            r = self.session.get(url, headers=headers)
+            if self.r.status_code == 200 or r.status_code == 206:
+                return r.content
+            else:
+                self.log.info(u"[INIT] Non-200 code while getting {}: {}".format(url, r.status_code))
+                raise FuseOSError(EIO)
+
+    def attributes(self):
+        self.log.debug(u"[ATTR] Attributes of file {}/{}".format(self.root, self.path))
+        print "=====Attributes of file root[{}], path[{}], is_filtered[{}]".format(self.root, self.path, self.is_filtered)
+
+        if not self.is_filtered and self.r.status_code != 200:
+            raise FuseOSError(ENOENT)
+
+        mode = (S_IFDIR | 0o777) if self.is_dir else (S_IFREG | 0o666)
+
+        attrs = {
+            'st_atime': self.mtime,
+            'st_mode': mode,
+            'st_mtime': self.mtime,
+            'st_size': self.size,
+        }
+
+        if self.is_dir:
+            attrs['st_nlink'] = 2
+
+        return attrs
+
+    def filter_path(self):
+        '''
+        To filter unqualified file
+        :return:
+        False - don't filter, keep processing
+        True - filter this file, don't need to further process
+        '''
+        rlt = False
+        name = self.path.split('/')[-1]
+        for w in ['.', 'DCIM', 'Gemfile', 'HEAD']:
+            if name.startswith(w):
+                rlt = True
+
+        print "=====filter_path: root[{}], path[{}], name[{}], rlt[{}]".format(self.root, self.path, name, rlt)
+
+        return rlt
